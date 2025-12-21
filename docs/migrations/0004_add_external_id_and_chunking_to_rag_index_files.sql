0f1f7bd88b4d40f50805513ef665e70501831468
Yakov <yakov@panargin.ru>
2025-12-21 11:32:50 +0300

    fix: добавлено удаление колонки chunking_strategy в миграцию
    - Миграция теперь корректно удаляет колонку chunking_strategy из rag_index_files
    - Добавлено DROP COLUMN IF EXISTS для безопасного удаления
    - Обновлен комментарий миграции для отражения всех изменений

--- docs/migrations/0004_add_external_id_and_chunking_to_rag_index_files.sql
+++ docs/migrations/0004_add_external_id_and_chunking_to_rag_index_files.sql
@@ -1,8 +1,13 @@
--- Добавление поля external_id в rag_index_files
+-- Добавление поля external_id и удаление chunking_strategy в rag_index_files
 -- Миграция: 0004_add_external_id_and_chunking_to_rag_index_files.sql
 
+-- Добавляем поле external_id
 ALTER TABLE rag_index_files 
 ADD COLUMN external_id VARCHAR(256) NULL;
 
--- Создание индекса для external_id для ускорения запросов
+-- Создаем индекс для external_id для ускорения запросов
 CREATE INDEX idx_rag_index_files_external_id ON rag_index_files(external_id);
+
+-- Удаляем поле chunking_strategy (теперь хранится в rag_files)
+ALTER TABLE rag_index_files 
+DROP COLUMN IF EXISTS chunking_strategy;
